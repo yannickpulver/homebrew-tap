3ea0d4a4d5100322e400a6fdfde39b637aadcbe2
Yannick Pulver <yannick@appswithlove.com>
2026-04-17 15:17:56 +0200

    raw-viewer 0.3.3 sha256 fix

--- Casks/raw-viewer.rb
+++ Casks/raw-viewer.rb
@@ -1,6 +1,6 @@
 cask "raw-viewer" do
   version "0.3.3"
-  sha256 "29f67ad623995a485716dd52298ce8ef9bb657f13d1ab4446e9fd68995463063"
+  sha256 "8ad3d3956e23bc453aba5c5ac9a4ecc37bc47a650e92a9936a7c122b206fd235"
 
   url "https://github.com/yannickpulver/raw-viewer/releases/download/v#{version}/RAW-Viewer.zip"
   name "RAW Viewer"
